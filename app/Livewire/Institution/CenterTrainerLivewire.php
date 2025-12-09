f8fe3b921e2a7e5716b33436c9a545c3f2912a0e
ManoguidKim <manoguidk@bayambang.gov.ph>
2025-12-09 14:35:22 +0800

    Add Livewire components and Blade views for Center and CenterTrainer for Institution

---
+++ app/Livewire/Institution/CenterTrainerLivewire.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Institution;
+
+use Livewire\Component;
+
+class CenterTrainerLivewire extends Component
+{
+    public function render()
+    {
+        return view('livewire.institution.center-trainer-livewire');
+    }
+}
