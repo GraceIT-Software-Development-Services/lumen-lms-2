97c449d1448213233d1d35f33bee6c708c46038b
ManoguidKim <manoguidk@bayambang.gov.ph>
2026-01-06 17:17:43 +0800

    Implement Training Batch Student Management: Add CRUD functionality, validation requests, and views for training batch students

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 use Modules\CourseAdministration\Http\Controllers\TrainingBatchController;
+use Modules\CourseAdministration\Http\Controllers\TrainingBatchStudentController;
 use Modules\CourseAdministration\Http\Controllers\TrainingCourseController;
 use Modules\Institution\Http\Controllers\CenterController;
 
@@ -14,15 +16,17 @@ Route::get('/', function () {
 
 Route::get('/', [PageController::class, 'index'])->name('landing');
 
-Route::get('/flow', function () {
-    return view('flow', ['title' => 'Flowbite Test']);
-})->name('flow');
+// Route::get('/flow', function () {
+//     return view('flow', ['title' => 'Flowbite Test']);
+// })->name('flow');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
     // Institution Module Routes
     Route::get('/centers', [CenterController::class, 'index'])->name('centers.index');
@@ -40,7 +44,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/training-courses/create', [TrainingCourseController::class, 'create'])->name('training_courses.create');
     Route::post('/training-courses/store', [TrainingCourseController::class, 'store'])->name('training_courses.store');
     Route::get('/training-courses/{uuid}', [TrainingCourseController::class, 'show'])->name('training_courses.show');
-    // Route::get('/training-courses/{uuid}/edit', [TrainingCourseController::class, 'edit'])->name('training_courses.edit');
+    Route::get('/training-courses/{uuid}/edit', [TrainingCourseController::class, 'edit'])->name('training_courses.edit');
     Route::put('/training-courses/{uuid}/update', [TrainingCourseController::class, 'update'])->name('training_courses.update');
     Route::delete('/training-courses/{uuid}/delete', [TrainingCourseController::class, 'destroy'])->name('training_courses.destroy');
     // ---- Training Batches ----
@@ -48,12 +52,21 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/training-batches/create', [TrainingBatchController::class, 'create'])->name('training_batches.create');
     Route::post('/training-batches/store', [TrainingBatchController::class, 'store'])->name('training_batches.store');
     Route::get('/training-batches/{uuid}', [TrainingBatchController::class, 'show'])->name('training_batches.show');
-    // Route::get('/training-batches/{uuid}/edit', [TrainingBatchController::class, 'edit'])->name('training_batches.edit');
+    Route::get('/training-batches/{uuid}/edit', [TrainingBatchController::class, 'edit'])->name('training_batches.edit');
     Route::put('/training-batches/{uuid}/update', [TrainingBatchController::class, 'update'])->name('training_batches.update');
     Route::delete('/training-batches/{uuid}/delete', [TrainingBatchController::class, 'destroy'])->name('training_batches.destroy');
+    // ---- Training Student Batches ----
+    Route::get('/training-student-batches', [TrainingBatchStudentController::class, 'index'])->name('training_student_batches.index');
+    Route::get('/training-student-batches/create', [TrainingBatchStudentController::class, 'create'])->name('training_student_batches.create');
+    Route::post('/training-student-batches/store', [TrainingBatchStudentController::class, 'store'])->name('training_student_batches.store');
+    Route::get('/training-student-batches/{uuid}', [TrainingBatchStudentController::class, 'show'])->name('training_student_batches.show');
+    Route::get('/training-student-batches/{uuid}/edit', [TrainingBatchStudentController::class, 'edit'])->name('training_student_batches.edit');
+    Route::put('/training-student-batches/{uuid}/update', [TrainingBatchStudentController::class, 'update'])->name('training_student_batches.update');
+    Route::delete('/training-student-batches/{uuid}/delete', [TrainingBatchController::class, 'destroy'])->name('training_student_batches.destroy');
 
 
     // Perfomance Administration Module Routes
+    // ---- To be added -----
 
 
 
