f9c81acf75a829fa641650a574bc838a994b27e0
ManoguidKim <manoguidk@bayambang.gov.ph>
2026-02-25 15:14:19 +0800

    feat: Remove 'uncompromised' rule from password validation in CreateUserRequest

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -59,7 +59,6 @@ class CreateUserRequest extends FormRequest
                     ->mixedCase()
                     ->numbers()
                     ->symbols()
-                    ->uncompromised(),
             ],
             'role' => [
                 'required',
